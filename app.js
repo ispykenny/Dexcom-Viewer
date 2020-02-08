a8d7e09840f910b4151b72006404cd6828fc3a2b
ispykenny <kenny.krosky@gmail.com>
2020-02-08 11:05:51 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const moment = require('moment');
+const PORT = 5000 || PORT;
+const dexcom = require('dexcom-share');
+const chalk = require('chalk');
+let allReadings = [];
+
+app.use('/' , (req, res, err) => {
+  res.send(allReadings)
+})
+
+
+
+
+const changedNumber = allReadings => {
+	if(allReadings.length >= 2) {
+    let previousItem = allReadings[allReadings.length - 1].reading;
+    let lastNumber = allReadings[allReadings.length - 2].reading;
+    
+    if(previousItem < lastNumber) {
+      difference = `-${lastNumber - previousItem} ↘`;
+    } else if(previousItem === lastNumber ) {
+      difference = `No change 🤙`;
+    } else {
+      difference = `+${previousItem - lastNumber} ↗`;
+    } 
+    return difference;
+  } else {
+    difference = 0;
+    return difference;
+  }
+}
+
+
+const fetchReadings = async () => {
+  const dexcomUser = dexcom({
+    username: process.env.USER_NAME,
+    password: process.env.USER_PASSWORD
+  })
+  
+  for await (const reading of dexcomUser) {
+    allReadings.push({
+      'reading': reading.Value,
+      'time': moment(reading.Date).format('MMMM Do YYYY, h:mm:ss a')
+    });
+    showResults(allReadings);
+    
+  }
+}
+  
+fetchReadings().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
+
+
+const showResults = allReadings => {
+  const lineBreak = '-----------------------------------'
+  console.log(lineBreak);
+  console.log(chalk.inverse(chalk.cyanBright('Last Reading:', chalk.white(chalk.bold(allReadings[allReadings.length - 1].reading)))));
+  console.log(chalk.green('Timestamp:', allReadings[allReadings.length - 1].time));
+  console.log(chalk.cyanBright('Last Change: ', changedNumber(allReadings)))
+}
+
+
+
+
+
+
+app.listen(PORT,  () => {
+  console.log(`listening on port: ${PORT}`)
+})
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Config
+--------------------------
+1) Create a .env file
+
+2) Enter your Dexcom Shared Login info with the following variable names
+   - USER_NAME=username
+   - USER_PASSWORD=password
+
+3) Run the app.js file
+
+![Dexcom Reader in terminal](https://kennykrosky.com/wp-content/uploads/2020/02/dexcomreader.png)
